0b1e84116fc77e9f2547420626250f945d289777
Christian Graber <chrisg@goya.attlocal.net>
2016-01-20 22:29:23 -0800

    Add page to add new wine

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -34,6 +34,18 @@ def showWines(varietal_id):
 def showWine(wine_id):
     wine = session.query(Wine).filter_by(id=wine_id).one()
     return render_template('description.html', wine=wine)
+
+@app.route('/catalog/add/', methods=['GET', 'POST'])
+def addWine():
+    if request.method == 'POST':
+        print "POST"
+        print request.form
+        varietal = session.query(Varietal).filter_by(name=request.form['varietal']).one()
+        newWine = Wine(name=request.form['name'], year=request.form['year'], varietal_id=varietal.id)
+        return redirect(url_for('addWine'))
+    else:
+        varietals = session.query(Varietal).all()
+        return render_template('addwine.html', varietals=varietals)
 
 if __name__ == '__main__':
     app.debug = True
